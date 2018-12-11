ddd628885cd3b8e91fff0686d8601598e383245d
isilia03 <melo.1995@hotmail.fr>
2018-12-11 11:08:38 +0100

    Add files via upload

---
+++ AffichageInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AffichageInteraction : MonoBehaviour {
+
+	public float intervalleInferieur;
+	public float intervalleSuperieur;
+	public GameObject informations;
+	public GameObject camera;
+
+	void Update () {
+		
+		if ((camera.transform.eulerAngles.y >= intervalleInferieur) && (camera.transform.eulerAngles.y <= intervalleSuperieur) && (Input.GetMouseButtonDown (0))) {
+			if (informations.activeInHierarchy == false) {
+				informations.SetActive (true);
+			} 
+
+			else {
+				informations.SetActive (false);
+			}
+		}
+	}
+}
